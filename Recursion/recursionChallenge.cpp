ef3f1505e540685cd0a31330d0c39a4302da7bf1
jacobsanosh <jacobsanosh@gmail.com>
2023-06-04 00:06:30 +0530

    maze

--- Recursion/recursionChallenge.cpp
+++ Recursion/recursionChallenge.cpp
@@ -31,7 +31,46 @@ void replace_string(string s)
         replace_string(s.substr(1));
     }
 
-}   
+} 
+void subeq(string s,string ans)
+{
+    if(s.length()==0)
+    {
+        cout<<ans<<endl;
+        return;
+    }
+    char ch=s[0];
+    int code=ch;
+    subeq(s.substr(1),ans);
+    subeq(s.substr(1),ans+ch);
+    subeq(s.substr(1),ans+to_string(code));
+} 
+bool issafe(int **arr,int x,int y,int n)
+{
+    if(x<n & y<n &&arr[x][y]==1)
+        return true;
+    return false;
+} 
+bool ratinmaze(int **arr,int x,int y,int n,int **solarr)
+{
+    if((x==(n-1))&& (y==(n-1))){
+        solarr[x][y]=1;
+        return true;
+    }
+    if(issafe(arr,x,y,n)){
+        solarr[x][y]=1;
+        if(ratinmaze(arr,x+1,y,n,solarr))
+        {
+            return true;
+        }
+        if(ratinmaze(arr,x,y+1,n,solarr))
+            return true;
+        solarr[x][y]=0;
+        return false;
+
+    }
+    return false;
+}
 int main()
 {
     //check array elemets ares sorted using recursion
@@ -48,7 +87,46 @@ int main()
    getline(cin,s);
    reverse_str(s);*/
    //replace pi with values ppixpppi ->p2.14xp3.14
+    /* 
    string s;
    getline(cin,s);
    replace_string(s);
+    */
+   /*string s;
+   getline(cin,s);
+   subeq(s,"");*/
+   //array maze problem
+   int n;
+   cin>>n;
+   int **arr=new int*[n];
+   for(int i=0;i<n;i++){
+    arr[i]=new int[n];
+   }
+   for(int i=0;i<n;i++)
+   {
+    for(int j=0;j<n;j++){
+        cin>>arr[i][j];
+    }
+   }
+   int **solarr=new int *[n];
+   for(int i=0;i<n;i++)
+   {
+        solarr[i]=new int[n];
+        for(int j=0;j<n;j++)
+        {
+            solarr[i][j]=0;
+        }
+   }
+    cout<<"\nsolution array is"<<endl;
+    if(ratinmaze(arr,0,0,n,solarr))
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++){
+                cout<<solarr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    
 }
