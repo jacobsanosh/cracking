0b97ce8ce3b957f2f42362d3b1e680a0dfe1447c
jacobsanosh <jacobsanosh@gmail.com>
2023-08-20 18:06:17 +0530

    upddating

---
+++ practice/check.py
@@ -0,0 +1,13 @@
+from collections import deque
+que=deque([1,2,3])
+print(que[len(que)-1])
+
+lst=[]
+lst.append(list(que))
+lst.append(list([1,2,3]))
+print(lst)
+
+for i in range(0):
+    print(i)
+
+print(False and True)
